dfe97c2346f5227bf9f6be205d0108d70db4f733
Shyam Sarkar <amisam15sarkar05@gmail.com>
2022-01-01 17:37:24 +0530

    test11

--- delete_data.php
+++ delete_data.php
@@ -2,6 +2,12 @@
 require "connection.php";
 $id = addslashes(trim($_POST['id']));
 if ($id > 0) {
+   $sql_old = "SELECT imgname FROM `contact_details` WHERE contact_id = '$id'";
+   $result = $conn->query($sql_old);
+   $imgname = $result->fetch_assoc();
+   if ($imgname['imgname'] != "") {
+      @unlink("uploaded/$imgname");
+   }
    $sql = "DELETE FROM `contact_details` WHERE contact_id = '$id'";
    $conn->query($sql);
    echo 1;
